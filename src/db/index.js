9bcf069b4160a27b63469592e9903f1a7b430dcb
mike-bds <rchallapa@ext.bancodelsol.com>
2021-10-30 23:16:33 -0300

    config dotenv and add evironments

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
+require('dotenv').config()
+const DB_NAME = process.env.DB_NAME
+const DB_PASSWORD = process.env.DB_PASSWORD
 
-const url = 'mongodb+srv://test-node:P0YZ8PDrfRSsV65Y@cluster0.sikq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const url = `mongodb+srv://${DB_NAME}:${DB_PASSWORD}@cluster0.sikq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const connectDb = async () => {
   try {
